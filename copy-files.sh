6bac1d1efd1fbf7c63b3176bb213bde514f4959c
Simen A. W. Olsen <so@bjerk.io>
2022-12-04 17:55:46 +0100

    find self dir

--- copy-files.sh
+++ copy-files.sh
@@ -3,7 +3,8 @@
 # Base directory
 BASE_DIR=/mnt/disks/gce-containers-mounts/gce-persistent-disks/map-disk
 CONFIG_PATH=$BASE_DIR/server/ShooterGame/Saved/Config/LinuxServer
-SELF_DIR=$(realpath "$0")
+SELF_SCRIPT=$(realpath "$0")
+SELF_DIR="${SELF_SCRIPT%/*}"
 
 cp $SELF_DIR/Game.ini $CONFIG_PATH/Game.ini
 cp $SELF_DIR/GameUserSettings.ini $CONFIG_PATH/GameUserSettings.ini
